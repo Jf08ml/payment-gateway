5aad0a1268d7a932600808515894c408a9a1835e
Juanfe <lassojuanfe@gmail.com>
2024-02-08 08:42:00 -0500

    Adjust frontend url api

--- frontend/src/services/paymentService.js
+++ frontend/src/services/paymentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "http://https://api-payment-gateway.vercel.app/api";
 
 const createPayment = async (paymentData) => {
   try {
